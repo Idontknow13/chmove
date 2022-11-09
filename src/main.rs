26f61fc51aa21334f3151c93e3fecc35a9059ba8
Idontknow13 <pokeminecraftexpert@gmail.com>
2022-11-09 23:37:18 +0800

    :broom: API clean up + changed name to `chwalk`

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ use std::{
 #[derive(Parser, Debug)]
 #[command(about)]
 struct Cli {
-    /// The amount of units the character will cross through
-    #[arg(short = 'd', long)]
+    /// The amount of units the character will traverse
+    #[arg(short = 'd', long, default_value_t = 5)]
     distance: usize,
 
     /// The speed of the character in milliseconds
@@ -26,18 +26,20 @@ struct Cli {
     speed: u64,
 
     /// Provide a custom character to animate
-    #[arg(short, long = "char")]
-    character: Option<char>,
+    #[arg(short, long = "char", default_value_t = '*')]
+    character: char,
 }
 
 #[doc(hidden)]
 fn main() {
     let args = Cli::parse();
 
-    let config = AnimationConfig::new(args.character, args.distance);
+    let config = AnimationConfig::init()
+        .with_char(args.character)
+        .until(args.distance);
 
     if config.move_with(args.speed).is_ok() {
-        println!("The asterisk reached its goal!");
+        println!("The character reached its goal!");
     }
 }
 
@@ -46,26 +48,35 @@ fn main() {
 /// A config struct containing all the needed context
 /// for the movement animation.
 struct AnimationConfig {
-    used_char: Option<char>,
+    character: char,
     distance: usize,
 }
 
 impl Default for AnimationConfig {
     fn default() -> Self {
         Self {
-            used_char: None,
+            character: '*',
             distance: 5,
         }
     }
 }
 
 impl AnimationConfig {
-    /// Create a new instance of `AnimationConfig`.
-    fn new(ch: Option<char>, distance: usize) -> Self {
-        Self {
-            used_char: ch,
-            distance,
-        }
+    /// Create a new instance of `AnimationConfig`
+    fn init() -> Self {
+        Self::default()
+    }
+
+    /// Specify a unique character for the config.
+    fn with_char(mut self, ch: char) -> Self {
+        self.character = ch;
+        self
+    }
+
+    /// Specify a traveling distance for the animation.
+    fn until(mut self, distance: usize) -> Self {
+        self.distance = distance;
+        self
     }
 
     /// Creates the moving animation for the asterisk given
@@ -82,7 +93,7 @@ impl AnimationConfig {
             println!(
                 "[{front_space}{character}{lead_space}]",
                 front_space = " ".repeat(traveled),
-                character = self.used_char.unwrap_or('*'),
+                character = self.character,
                 lead_space = " ".repeat(distance - traveled)
             );
             stdout().flush()?;
